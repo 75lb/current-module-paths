98cf0e967c7f97dbe9ac4e8a2676a2bbdb2dc56d
Lloyd Brookes <lloyd.brookes@gmail.com>
2023-05-02 13:16:43 +0100

    upgrade deps, dates and CI

--- test/test.mjs
+++ test/test.mjs
@@ -6,7 +6,7 @@ const a = assert.strict
 const tom = new TestRunner.Tom()
 
 tom.test('simple', async function () {
-  const { __filename, __dirname } = currentModulePaths(import.meta.url)
+  const { __filename, __dirname } = currentModulePaths('file:///Users/lloyd/Documents/75lb/current-module-paths/test.mjs')
   a.ok(/current-module-paths\/test.mjs/.test(__filename))
   a.ok(/current-module-paths/.test(__dirname))
 })
